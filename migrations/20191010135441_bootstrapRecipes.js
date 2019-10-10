7d8eec9ce901e438ec37f2e51851f35b6ae12467
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-10 14:29:23 -0600

    migration tables working

--- migrations/20191010135441_bootstrapRecipes.js
+++ migrations/20191010135441_bootstrapRecipes.js
@@ -1,47 +1,47 @@
 
 exports.up = function(knex) {
     return knex.schema
-    .createTable('recipes', tbl=>{
+    .createTable('recipesgood', tbl=>{
         tbl.increments();
   
         tbl.string('name', 255).notNullable();
     })
-    .createTable('ingredients', tbl=>{
+    .createTable('ingredientsgood', tbl=>{
       tbl.increments();
   
       tbl.string('name', 255).notNullable();
     })
-    .createTable('user_favorites', tbl=>{
-      tbl.increments();
-  
-      tbl.text('notes')
-  
-      //these next 2 columns add the foreign keys
-      tbl
-      .integer('recipes_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('recipes')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT')
-  
-      tbl
-      .integer('ingredients_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('ingredients')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT')
-  
-      tbl.unique(['recipes_id', 'ingredients_id'])
-    })
+    .createTable('recipe_ingredients', tbl=>{
+        tbl.increments();
+    
+        tbl.text('notes')
+    
+        //these next 2 columns add the foreign keys
+        tbl
+        .integer('recipesgood_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipesgood')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT')
+    
+        tbl
+        .integer('ingredientsgood_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredientsgood')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT')
+    
+        tbl.unique(['recipesgood_id', 'ingredientsgood_id'])
+      })
   };
 
 exports.down = function(knex) {
     return knex.schema
-  .dropTableIfExists('user_favorites')
-  .dropTableIfExists('users')
-  .dropTableIfExists('pois')
+    .dropTableIfExists('recipe_ingredients')
+  .dropTableIfExists('recipesgood')
+  .dropTableIfExists('ingredientsgood')
 };
