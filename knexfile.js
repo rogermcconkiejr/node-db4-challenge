70c879b537693f6971bcef660565531997dacd01
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-10 13:59:08 -0600

    knexfile created

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,17 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true, 
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/database.db3',
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
 
   staging: {
