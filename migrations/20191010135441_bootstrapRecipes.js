d017ecb8a1fbb29a4500f077cce03f9c160fe3d9
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-10 14:09:18 -0600

    knex migrate written

--- migrations/20191010135441_bootstrapRecipes.js
+++ migrations/20191010135441_bootstrapRecipes.js
@@ -1,8 +1,47 @@
 
 exports.up = function(knex) {
+    return knex.schema
+    .createTable('recipes', tbl=>{
+        tbl.increments();
   
-};
+        tbl.string('name', 255).notNullable();
+    })
+    .createTable('ingredients', tbl=>{
+      tbl.increments();
+  
+      tbl.string('name', 255).notNullable();
+    })
+    .createTable('user_favorites', tbl=>{
+      tbl.increments();
+  
+      tbl.text('notes')
+  
+      //these next 2 columns add the foreign keys
+      tbl
+      .integer('recipes_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT')
+  
+      tbl
+      .integer('ingredients_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('ingredients')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT')
+  
+      tbl.unique(['recipes_id', 'ingredients_id'])
+    })
+  };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+  .dropTableIfExists('user_favorites')
+  .dropTableIfExists('users')
+  .dropTableIfExists('pois')
 };
